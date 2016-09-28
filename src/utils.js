7a752c0579eff27426d5ddd532702865482f5b73
southrop <southrop113@gmail.com>
2016-09-29 01:07:04 +0800

    Work with everything in lower case and return original values

--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,21 @@
 'use strict';
 
-function toTitleCase(str) {
-  if (!str) {
-    return null;
-  }
-  if (str.constructor === Array) {
-    // Turn array of values into single string
-    str = str.join(', ');
-  }
-  return str.toLowerCase().replace(/\b\w/g, function (txt) {
-    return txt.charAt(0).toUpperCase();
-  });
-}
-
 function matchString(string, data) {
   let keys = Object.keys(data);
 
-  let key = keys.find((key) => {
+  // Convert everything to lowercase for case insensitivity
+  let lowerCaseKeys = keys.map((str) => {
+    return str.toLowerCase();
+  });
+  let index = lowerCaseKeys.findIndex((key) => {
     return string.toLowerCase().indexOf(key) !== -1;
   });
 
-  return toTitleCase(data[key]);
+  let nickname = data[keys[index]];
+  if (nickname && nickname.constructor === Array) {
+    nickname = nickname.join(', ');
+  }
+  return nickname;
 }
 
-module.exports.toTitleCase = toTitleCase;
 module.exports.matchString = matchString;
